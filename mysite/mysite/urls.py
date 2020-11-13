cbbd18e3dbb391b390478d2aabe998c9d914722f
grizzlyui <Muller@1996>
2020-11-13 14:11:27 +0200

    'userreg'

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users.views import register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', register_view, name="register"),
     path('blog/', include('blog.urls')),
 ]
