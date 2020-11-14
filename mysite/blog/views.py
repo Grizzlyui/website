d141553f3b10e6091a1b26de71ec67acc9d67320
Grizzly <b.mullerjnr@gmail.com>
2020-11-14 18:26:19 +0200

    'posts'

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
+from django.views.generic import (ListView, 
+    DetailView, 
+    CreateView
+)
 from django.http import  HttpResponse
 from .models import Post
 
-# Create your views here.
+
 def home_view(request):
     context = {
         'posts' : Post.objects.all()
     }
     return render(request, "blog/home.html", context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    #queryset = posts.objects.order_by('-date_posted')
+    ordering = ['-id']
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
 
 def about_view(request):
     return render(request, "blog/about.html", {'title': 'About'})
