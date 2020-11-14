42ec30d87d6e25751bdbacdf999c55ab9c477cce
Grizzly <b.mullerjnr@gmail.com>
2020-11-14 08:48:08 +0200

    'profilesupdated'

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,11 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from users.views import register_view
+from django.conf import settings
+from django.conf.urls.static import static
+from users.views import register_view, profile_view
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', register_view, name="register"),
-    path('blog/', include('blog.urls')),
-]
+    path('login/', auth_views.LoginView.as_view(template_name = "users/login.html"), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name = "users/logout.html"), name="logout"),
+    path('register/', register_view, name="register"),
+    path('profile/', profile_view, name="profile"),
+    path('', include('blog.urls')),
+]  
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
